96a3bc785f2f109fd0844988075e032a03e58e9a
vladislav454 <59504838+vladislav454@users.noreply.github.com>
2023-04-16 23:30:32 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,46 @@
-# python3
+B = 31
+Q = 1000211 # to avoid overflow
+
+def get_hash(s):
+    hash_val = 0
+    for i in range(len(s)):
+        hash_val = (hash_val * B + ord(s[i])) % Q
+    return hash_val
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
-    
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    input_type = input().rstrip()
+    if input_type == 'I':
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif input_type == 'F':
+        with open(f"./tests/{file_path}", "r") as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
+
+    return pattern, text
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    occurrences = []
+    pattern_len = len(pattern)
+    text_len = len(text)
 
-    # and return an iterable variable
-    return [0]
+    multiplier = 1
+    for i in range(1, pattern_len):
+        multiplier = (B * multiplier) % Q
 
+    pattern_hash = get_hash(pattern)
+    text_hash = get_hash(text[:pattern_len])
 
-# this part launches the functions
+    for i in range(text_len - pattern_len + 1):
+        if pattern_hash == text_hash and pattern == text[i:i+pattern_len]:
+            occurrences.append(i)
+        if i < text_len - pattern_len:
+            text_hash = (B * (text_hash - ord(text[i]) * multiplier) + ord(text[i + pattern_len])) % Q
+
+    return occurrences
+
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
